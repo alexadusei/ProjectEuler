dc933f77c9a42aab20b78b67f8dd8c55c93e7a64
Alexander Adusei <12aa100@queensu.ca>
2015-01-22 17:34:27 -0500

    Changed comment code spacing

--- q1.py
+++ q1.py
@@ -1,13 +1,13 @@
 #-------------------------------------------------------------------------------
 # Name:        Multiples of 3 and 5
-#
+
 # Purpose:     If we list all the natural numbers below 10 that are multiples
 #              of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23
 #              Find the summ of all the multiples of 3 or 5 below 1000
-#
+
+# Answer:      233168
+
 # Author:      Alex Adusei
-#
-# Created:     22-01-2015
 #-------------------------------------------------------------------------------
 
 import time
