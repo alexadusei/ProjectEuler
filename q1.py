1b5ce757c511f59ccc53e1769e2c67056cc77608
Alexander Adusei <12aa100@queensu.ca>
2015-01-22 17:30:52 -0500

    Update .py file

--- q1.py
+++ q1.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Name:        Multiple of 3 and 5
+# Name:        Multiples of 3 and 5
 #
 # Purpose:     If we list all the natural numbers below 10 that are multiples
 #              of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23
