adb4ff8574e3cbc294b16c076a7a8213caf49da2
Alexander Adusei <12aa100@queensu.ca>
2015-01-22 17:13:47 -0500

    Updated .py file

--- q1.py
+++ q1.py
@@ -1,5 +1,6 @@
 #-------------------------------------------------------------------------------
 # Name:        Multiple of 3 and 5
+#
 # Purpose:     If we list all the natural numbers below 10 that are multiples
 #              of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23
 #              Find the summ of all the multiples of 3 or 5 below 1000
@@ -20,7 +21,4 @@ def main():
 
     print sumMultiples
 
-#Answer: 233168
-t = time.clock();
-main()
-print round(time.clock() - t, 6), "seconds process time"
+main() #Answer: 233168
