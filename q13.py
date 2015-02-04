aa91304d0ead2e15f2be0a368febe0d71873c2f3
Alexander Adusei <12aa100@queensu.ca>
2015-02-04 11:20:20 -0500

    Uploaded text file and .py script

---
+++ q13.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        Large Sum
+
+# Purpose:     Work out the first first ten digits of the sum of the following
+#              one-hundred 50-digit numbers.
+
+# Answer:      5537376230
+
+# Author:      Alex Adusei
+#-------------------------------------------------------------------------------
+
+# Helper function to read large number from textfile
+def readNums():
+    file = open("q13.txt")
+    nums = []
+
+    for line in file:
+        line = line.strip()
+        nums += [int(line)]
+
+    return nums
+
+numbers = readNums()
+print str(sum(numbers))[:10]
