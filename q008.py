e117c50eea32fb3ce1e46cee59159e0478cc2264
Alexander Adusei <12aa100@queensu.ca>
2015-02-08 02:02:29 -0500

    Updated .py module

--- q008.py
+++ q008.py
@@ -2,7 +2,7 @@
 # Name:        Largest Product in a Series
 
 # Purpose:     The four adjacent digits in the 1000-digit number (provided in
-#              textfile 'q8.txt')  that have the greatest product are
+#              textfile 'q008.txt')  that have the greatest product are
 #              9 x 9 x 8 x 9 = 5832. Find the thirteen adjacent digits in the
 #              1000-digit number that have the greatest product. What is the
 #              value of this product?
