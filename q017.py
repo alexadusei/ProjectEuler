146a723af2797eab1c0553cf4b629359cfe25d49
Alexander Adusei <12aa100@queensu.ca>
2015-02-12 08:10:25 -0500

    Optimized code

--- q017.py
+++ q017.py
@@ -16,48 +16,40 @@
 # Author:      Alex Adusei
 #-------------------------------------------------------------------------------
 
-digits = ["", "one", "two", "three", "four", "five", "six", "seven", "eight",
-            "nine"]
+digits = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 # 'ten' is not a teen number, but kept here for the sake of simplicity
 teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
-             "seventeen", "eighteen", "nineteen"]
+            "seventeen", "eighteen", "nineteen"]
 
-tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty",
-        "ninety"]
+tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
 hundreds = "hundred"
 thousands = "thousand"
 conjunction = "and"
 numbers = []
-numSum = 0
 
 for i in range(1, 1000+1):
     num = str(i)
 
     while len(num) != 0:
 
-        if len(num) == 4:
+        if len(num) == 4: #add 'one thousand' to array and splice to nothing
             numbers += [digits[int(num[0])], thousands]
             num = num[3:]
-        if len(num) == 3:
+        if len(num) == 3: #add 'x hundred (and)' to array and skip digit based on 0-index
             if num[1] == '0' and num [2] == '0':
                 numbers += [digits[int(num[0])], hundreds]
                 num = num[2:]
-            elif num[1] == '0':
-                numbers += [digits[int(num[0])], hundreds, conjunction]
-                num = num[1:]
             else:
                 numbers += [digits[int(num[0])], hundreds, conjunction]
-        elif len(num) == 2:
-            if num[0] == '1':
-                numbers += [teens[int(num[1])]]
-                num = num[1:]
-            else:
-                numbers += [tens[int(num[0])-2]]
-        else:
+                num = num[1:] if num[1] == '0' else num
+        elif len(num) == 2: #add teen digits or tens digits to array based on 1-index
+            numbers += [teens[int(num[1])]] if num[0] == '1' else [tens[int(num[0])-2]]
+            num = num[1:] if num[0] == '1' else num
+        else: #add single digit to array if all other cases fail
             numbers += [digits[int(num[0])]]
 
-        num = num[1:]
+        num = num[1:] #go to next digit of number
 
 print sum([len(numbers[i]) for i in range(len(numbers))])
