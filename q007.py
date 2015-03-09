3ac310a5bedc25375b46ce1b3db8672ea4913b31
Alex Adusei <12aa100@queensu.ca>
2015-03-08 21:31:32 -0400

    Update q007.py

--- q007.py
+++ q007.py
@@ -2,7 +2,7 @@
 # Name:        1001st Prime
 
 # Purpose:     By listing the first six prime numbers: 2, 3, 5, 7, 11 and 13, we
-#              can see that the 6th prime is 13. What is teh 10001st prime
+#              can see that the 6th prime is 13. What is the 10001st prime
 #              number?
 #
 # Answer:      104743
